6ab8afe75761c7f1869cf67555d0b86b83ce52e4
Ritik Sahni <ritiksahni0203@gmail.com>
2023-09-26 15:20:45 +0530

    Change winston config

--- config/logger.js
+++ config/logger.js
@@ -1,9 +1,17 @@
 const winston = require("winston");
+const { combine, timestamp, printf, colorize, align } = winston.format;
 
 const logger = winston.createLogger({
-    level: "debug",
-    format: winston.format.json(),
+    level: process.env.LOG_LEVEL || "info",
+    format: combine(
+        colorize({ all: true }),
+        timestamp({
+            format: "YYYY-MM-DD hh:mm:ss.SSS A",
+        }),
+        align(),
+        printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+    ),
     transports: [new winston.transports.Console()],
 });
 
-module.exports = logger;
+module.exports = { logger };
