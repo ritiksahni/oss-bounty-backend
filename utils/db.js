0811ae43cd52f623d81952b0987e222d81ef8bda
Ritik Sahni <ritiksahni0203@gmail.com>
2023-09-30 01:50:46 +0530

    Use environment variables for keys

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
+require("dotenv").config();
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "test",
-    database: "oss_bounty",
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
 });
 
 module.exports = conn;
--- utils/oauth.js
+++ utils/oauth.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 const config = {
     authRequired: false,
     auth0Logout: true,
-    baseURL: "http://localhost:3000",
-    clientID: "wgq0nkCDZAcEsy9flHLk3zg4JJAa3nIX",
-    issuerBaseURL: "https://dev-brolnzetfm53prgv.us.auth0.com",
-    secret: "2U1E-w97QHq06_yll324_vYxWIfX8C_l44wrx_cF6dYZ3_vq7pwy6norszvh43dm",
+    baseURL: process.env.HOST_BASE_URL || "http://localhost:3000",
+    clientID: process.env.AUTH0_CLIENT_ID,
+    issuerBaseURL: process.env.AUTH0_BASE_URL,
+    secret: process.env.AUTH0_SECRET,
 };
 
 module.exports = config;
