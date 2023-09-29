7c15cdf023d87672bab30a95ebfa671e55370032
Ritik Sahni <ritiksahni0203@gmail.com>
2023-09-29 22:45:06 +0530

    Add utility for GitHub data fetching

---
+++ utils/githubFetcher.js
@@ -0,0 +1,34 @@
+const { Octokit } = require("@octokit/core");
+
+const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+});
+
+// Data: data.name, data.owner.login, description, data.language
+
+function parseGithubUrl(githubUrl) {
+    let splitUrl = new URL(githubUrl).pathname.split("/");
+    splitUrl = splitUrl.filter((item) => item !== ""); // remove empty strings
+
+    return { owner: splitUrl[0], repoName: splitUrl[1] };
+}
+
+async function getRepoData(repoOwner, repoName) {
+    const data = {};
+    const repoData = await octokit.request("GET /repos/{owner}/{repo}", {
+        owner: repoOwner,
+        repo: repoName,
+        headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+        },
+    });
+
+    data["name"] = repoData.data.name;
+    data["owner"] = repoData.data.owner.login;
+    data["description"] = repoData.data.description;
+    data["language"] = repoData.data.language;
+
+    console.log(data);
+}
+
+module.exports = { parseGithubUrl, getRepoData };
