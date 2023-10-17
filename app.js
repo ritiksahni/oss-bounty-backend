0c655c06ccbe9ce864ba337b6faa528fac3912cc
Ritik Sahni <ritiksahni0203@gmail.com>
2023-10-17 16:52:27 +0530

    Enable CORS for all routes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const claimRoutes = require("./routes/claimRoutes");
 // Library Imports
 const { auth } = require("express-openid-connect");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
@@ -16,10 +17,13 @@ const port = 3000;
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 app.use(express.json());
+app.use(cors()); // Enable CORS for all routes.
 
 // req.isAuthenticated is provided from the auth router
 app.get("/", (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+    if (req.oidc.isAuthenticated) {
+        res.redirect("http://localhost:5173");
+    }
 });
 
 // Mounting Routes
